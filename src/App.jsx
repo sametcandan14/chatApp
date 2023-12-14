e53e1d5d2bc2131b26e9f5372105b5e4c216ee10
Samet Candan <c._samet@hotmail.com>
2023-12-14 23:30:33 +0300

    logout and chat page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,54 @@
 import Auth from "./pages/Auth";
+import { auth } from "./firebase/firebaseConfig";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useEffect, useState } from "react";
+import Chat from "./pages/Chat";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(true);
+  const [room, setRoom] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuth(true);
+      } else {
+        setIsAuth(false);
+      }
+    });
+  }, []);
+
+  const handleLogOut = () => {
+    signOut(auth).catch((err) => console.log(err));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setRoom(e.target[0].value);
+  };
+
+  if (!isAuth) {
+    return (
+      <div className="container">
+        <Auth />
+      </div>
+    );
+  }
   return (
     <div className="container">
-      <Auth />
+      {room ? (
+        <Chat room={room} />
+      ) : (
+        <form onSubmit={handleSubmit} className="room-container">
+          <h1>Chat Room</h1>
+          <p>Select a Room</p>
+          <input type="text" />
+          <button type="submit">Enter Room</button>
+          <button onClick={handleLogOut} className="logout" type="button">
+            Log Out
+          </button>
+        </form>
+      )}
     </div>
   );
 }
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,5 +1,18 @@
-const Chat = () => {
-  return <div>Chat</div>;
+const Chat = ({ room }) => {
+  return (
+    <div className="chat">
+      <header>
+        <p>Samet</p>
+        <p>{room}</p>
+        <a href="/">Change Room</a>
+      </header>
+      <main></main>
+      <form action="">
+        <input placeholder="type your message..." type="text" />
+        <button>Send</button>
+      </form>
+    </div>
+  );
 };
 
 export default Chat;
